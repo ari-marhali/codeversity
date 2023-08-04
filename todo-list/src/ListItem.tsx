c2ea70f81891c19846515ffde13abb0f65d5ac61
ari-ma27 <ari.marhali@gmail.com>
2023-08-04 15:31:11 +0200

    Update ListItem.tsx

--- todo-list/src/ListItem.tsx
+++ todo-list/src/ListItem.tsx
@@ -1,33 +1,45 @@
-import {useState} from 'react'
+import { useState } from "react";
 
 type ListItemProps = {
-    item:{
-        id: number;
-        done: boolean;
-        text: string;
-    }
-    checkboxHandler: (id:number) => void
-    deleteHandler: (id:number) => void
+  item: {
+    id: number;
+    done: boolean;
+    text: string;
+  };
+  checkboxHandler: (id: number) => void;
+  deleteHandler: (id: number) => void;
+};
+
+function ListItem(props: ListItemProps) {
+  const [hover, setHover] = useState<boolean>(false);
+
+  return (
+    <div
+      className="list-item"
+      onMouseOver={() => setHover(true)}
+      onMouseOut={() => setHover(false)}
+    >
+      <label id="checkbox">
+        <input
+          type="checkbox"
+          id="done-btn"
+          checked={props.item.done}
+          onChange={() => props.checkboxHandler(props.item.id)}
+        />
+        <span id="checkmark"></span>
+      </label>
+      <p id="item-txt">{props.item.text}</p>
+      {hover && (
+        <button
+          id="remove-btn"
+          onClick={() => props.deleteHandler(props.item.id)}
+        >
+          🗑️
+        </button>
+      )}
+    </div>
+  );
 }
 
-
-function ListItem (props: ListItemProps){
-
-    const [hover, setHover]= useState<boolean>(false)
-
-    return(
-        <div className="list-item" onMouseOver={()=>setHover(true)} onMouseOut={()=>setHover(false)} >
-            <label id='checkbox'> 
-                <input type="checkbox" id='done-btn' checked={props.item.done} onChange = {() => props.checkboxHandler(props.item.id)} />
-                <span id='checkmark'></span>
-            </label>
-            <p id='item-txt'>{props.item.text}</p>
-            {hover && <button id='remove-btn' onClick={()=> props.deleteHandler(props.item.id)}>🗑️</button>}
-        </div>
-
-    )
-
-}
-
-export {ListItem}
-export type {ListItemProps}
+export { ListItem };
+export type { ListItemProps };
