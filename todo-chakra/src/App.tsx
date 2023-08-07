a4286cb6323327b95432d06a4128b63293c56f2e
ari-ma27 <ari.marhali@gmail.com>
2023-08-07 16:22:28 +0200

    chakra empty

--- todo-chakra/src/App.tsx
+++ todo-chakra/src/App.tsx
@@ -1,14 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { Box } from "@chakra-ui/react";
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-
-  )
+  return <Box>Hello World</Box>;
 }
 
-export default App
+export default App;
